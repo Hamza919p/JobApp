a176f8f32773c951cc2f3f0a302c7ec3ce042e91
Hamza-Ali <hamza.919p@gmail.com>
2024-07-25 16:20:37 +0200

    added welcome and popular jobs ui

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { router } from 'expo-router';
+import styles from './welcome.style';
+import {COLORS, icons, SIZES } from '../../../constants'
 
-import styles from './welcome.style'
+const jobTypes = ['full-time', 'part-time', 'contractor'];
 
 const Welcome = () => {
+
+  const [activeJobType, setActiveJobType] = useState('full-time');
+
   return (
     <View>
-      <Text>Welcome</Text>
+
+      {/* User Details */}
+      <View style = { styles.container }>
+        <Text style = { styles.userName }>Hello Hamza!</Text>
+        <Text style = { styles.welcomeMessage }>Find your perfect Job</Text>
+      </View>
+
+      {/* Search Field */}
+      <View style = { styles.searchContainer } >
+        <View style = { styles.searchWrapper }>
+          <TextInput style = { styles.searchInput } value="" onChange={ ()=>{} } placeholder="What are you looking for?" placeholderTextColor={COLORS.gray} />
+        </View>
+        <TouchableOpacity style = {styles.searchBtn} onPress={ () => {}}> 
+            <Image source={icons.search} resizeMode='contain' style = {styles.searchBtnImage} ></Image>
+          </TouchableOpacity>
+      </View>
+
+      {/* Horizontal list of job types */}
+      <View style = {styles.tabsContainer} >
+        <FlatList 
+          data = {jobTypes} 
+          renderItem = { ( { item } ) => (
+            <TouchableOpacity style = {styles.tab(activeJobType, item)} onPress={ ()=> { setActiveJobType(item) } }>
+              <Text style = {styles.tabText(activeJobType, item)}> 
+                {item} 
+              </Text>
+            </TouchableOpacity>
+          ) } 
+          keyExtractor={ item => item }  //this is unique id of a specific item
+          contentContainerStyle = { { columnGap: SIZES.small  } }
+          horizontal
+        />
+      </View>
+
     </View>
   )
 }
